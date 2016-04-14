13a53111fa1cac0f0514869cb1ca2b8958a84641
Myla83 <lyudmylaluneva@gmail.com>
2016-04-14 15:08:17 +0300

    added homework7-8js_2

---
+++ homework7-8js_2/js7-8js_2.js
@@ -0,0 +1,39 @@
+$(function() {
+
+var $firstName = $('#firstname');
+var $lastName = $('#lastname');
+var $adress = $('#address');
+var $button = $('button');
+
+$firstName.hover(function(){$('.firstname').css({
+    opacity: 1
+
+})}, function() {$('.firstname').css({
+    opacity: 0
+
+})});
+$lastName.hover(function(){$('.lastname').css({
+    opacity: 1
+
+})}, function() {$('.lastname').css({
+    opacity: 0
+
+})});
+$adress.hover(function(){$('.address').css({
+    opacity: 1
+
+})}, function() {$('.address').css({
+    opacity: 0
+
+})});
+
+$button.click(function(e){
+e.preventDefault();
+        $('span').css({
+               opacity: 1
+          });
+
+console.log($button)
+  });
+
+});
