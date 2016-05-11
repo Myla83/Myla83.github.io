6a20e9b89be8a9ac2058571bfdc02ef60c4b5854
Myla83 <lyudmylaluneva@gmail.com>
2016-05-11 16:35:05 +0300

    added hw13-14

--- homework13-14/js/js13-14.js
+++ homework13-14/js/js13-14.js
@@ -1,33 +1,61 @@
-/*'use strict';*/
-/*var animal = {
-  name: 'Jack',
-  age: 11,
-  color: 'grey'
-};
-var str = JSON.stringify(animal);
-console.log(str);
+'use strict';
+var dataTest = [
+  {question: 'Вопрос № 1',
+   answer: [ 'Вариант ответа № 1', 'Вариант ответа № 2', 'Вариант ответа № 3'],
+   check: ['true', 'false', 'false'],
+   name: 'first'
+ },
+ {question: 'Вопрос № 2',
+  answer: [ 'Вариант ответа № 1', 'Вариант ответа № 2', 'Вариант ответа № 3'],
+  check: ['false', 'true', 'false'],
+  name: 'second'
+},
+{question: 'Вопрос № 3',
+ answer: [ 'Вариант ответа № 1', 'Вариант ответа № 2', 'Вариант ответа № 3'],
+ check: ['false', 'false', 'true'],
+ name: 'third'
+}];
 
-var obj = JSON.parse(str);
-console.log(obj);*/
+localStorage.setItem('myTest', JSON.stringify(dataTest));
 
-/*var dog = {
-  name: 'Bob',
-  age: 3,
-  color: 'black'
-};
+$(function() {
 
-localStorage.setItem('animal', JSON.stringify(dog));
-var test = localStorage.getItem('animal');
+  var test = $('#test').html();
 
-test = JSON.parse(test);
-console.log(test);*/
+  var getTest = localStorage.getItem('myTest');
+  getTest = JSON.parse(getTest);
 
-var str = '{name: "dad"}';
-try {
-var obj = JSON.parse(str);
-console.log('obj.name', obj.name);
-} catch (e) {
-  alert('error');
-} finally {
-  console.log('finaly');
-}
+  var content = tmpl(test, {
+    data: getTest
+  });
+  $('.check').before(content);
+
+  $('.check').on('click', function() {
+    var $result = true;
+    $('.checkbox').each(function() {
+      if ($(this).prop('checked') != ($(this).attr('value') == 'true')) {
+        $result = false;
+        return false;
+      };
+    //  $('.modal-window__button').css("display", "block").animate({opacity: 1}, 500);
+    });
+    console.log($result);
+
+    $('.modal-window__message')[0].innerHTML = $result ? 'Тест пройден' : 'Тест не пройден';
+    $('.modal-window').css("display", "block").animate({opacity: 1}, 500);
+  });
+
+
+    $('.modal-window__button').on('click', function(){
+      $('.modal-window').animate({opacity: 0}, 500, function() {
+        $(this).css("display", "none");
+      })
+      $('.checkbox').each(function() {
+        $(this).prop('checked', false);
+      });
+    });
+
+
+
+
+});
